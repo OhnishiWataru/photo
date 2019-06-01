dba4cfacedfb46423edc27848f58ad32561b3bf1
OhnishiWataru <ohnishi.wataru.business@gmail.com>
2019-06-01 18:02:15 +0900

    node

--- resources/views/admin/photo/edit.blade.php
+++ resources/views/admin/photo/edit.blade.php
@@ -73,7 +73,7 @@
           <div class="col-md-10">
             <input type="hidden" name="id" value="{{ $photo_form->id }}">
             {{ csrf_field() }}
-            <input type="submit" class="btn btn-primary" value="投稿">
+            <input type="submit" class="btn btn-primary" value="更新">
           </div>
         </div>
       </form>
